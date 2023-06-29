b970b49b9bf7cbeb5d72834737fb741e0ede2ea6
Chris Bowyer <100442597+Devcab2@users.noreply.github.com>
2023-06-28 21:55:22 -0600

    array.filter()

---
+++ array-filter().js
@@ -0,0 +1,38 @@
+// suppose you want to find certain elements that meet a condition and extract them into a new array
+// you could use a loop...
+
+const textScores = [72, 95, 90, 71, 75, 79];
+const arr = [];
+
+for (const element of textScores) {
+  if (element >= 90) {
+    arr.push(element);
+  }
+}
+
+console.log(arr);
+
+
+// instead we can use .filter() this creates a new array with values that satisfy a condition. 
+// arrayName.filter()
+
+const arr2 = textScores.filter(function(element) {
+  return element > 76;
+});
+
+console.log(arr2);
+
+// give the inline function access to the arrays elements by adding a parameter to the function
+
+// to tell the .filter() function which values to store in the new array the functions return statement must return a boolean
+
+// typically we write aa conditional involving the element or index that results in a boolean 
+
+// => example: const arr = textScores.filter(element => element >= 90);
+
+const names = ["Tony", "Tanis", "Vinny", "Terry"];
+
+const arrr = names.filter(element => (element[0] === "T"));
+
+console.log(arrr);
+
